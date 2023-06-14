b6c0721bceb02af8097e0e4e19ea57377e7d0a9b
Ly Phat Tai <lpt20xx@gmail.com>
2023-06-14 22:29:40 +0700

    Add Photon PUN AppID
    Add Photon PUN AppID

---
+++ FPSPun/Assets/_Script/Launcher/Launcher.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Joined Lobby");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
